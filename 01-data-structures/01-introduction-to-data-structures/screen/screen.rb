f27d10236074513090541814a1a0aeddf681d4f2
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-08-27 15:31:45 -0500

    line and screen data structure complete

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,4 +1,5 @@
 require_relative 'pixel'
+require 'matrix'
 
 class Screen
   attr_accessor :width
@@ -6,18 +7,43 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Matrix.build(height, width){|row, col| col - row}
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    self.matrix[x,y]=pixel
   end
 
   def at(x, y)
+    if inbounds(x,y)
+     value = self.matrix[x,y]
+     if value.kind_of? Pixel
+      return value
+     else
+      return nil
+     end
+    else
+     return nil
+    end
   end
 
   private
 
   def inbounds(x, y)
+    if x < 0 || y < 0
+      return false
+    else
+      return true
+    end
   end
 
+end
+
+class Matrix
+  def []=(i, j, x)
+    @rows[i][j] = x
+  end
 end
