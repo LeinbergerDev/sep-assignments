2a702d1015f11a27856142567df59f224dbbf568
jason@leinberberdev.com <jason@leinbergerdev.com>
2018-08-28 10:27:15 -0500

    checkpoint2 complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -3,16 +3,37 @@ class MyQueue
   attr_accessor :tail
 
   def initialize
+    @index = -1
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[@index]
   end
 
   def enqueue(element)
+    @index += 1
+    @queue.insert(@index, element)
+    @head = @queue[0]
+    @tail = @queue[@index]
   end
 
   def dequeue
+    if empty?
+     return nil
+    else
+     @index -= 1
+     value = @queue[0]
+     @queue.delete_at(0)
+     @head = @queue[0]
+     @tail = @queue[@index]
+     return value
+    end
   end
 
   def empty?
+    if @queue.length == 0
+      return true
+    else
+      return false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -3,15 +3,30 @@ class MyStack
 
   def initialize
     @stack = Array.new
+    @index = -1
     self.top = nil
   end
 
   def push(item)
+    @index += 1
+    @stack.insert(@index, item)
+    self.top = @stack[@index]
   end
 
   def pop
+    value = @stack[@index]
+    @stack.delete_at(@index)
+    @index -= 1
+    self.top = @stack[@index]
+    return value;
   end
 
   def empty?
+    if @index == -1
+      return true
+    else
+      return false
+    end
   end
+  
 end
